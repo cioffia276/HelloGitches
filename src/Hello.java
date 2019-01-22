fa854491872f5a518f9701e08703234d71e1b7e9
cioffia276 <cioffia276@ALB205-16.strose.edu>
2019-01-22 14:56:32 -0500

    Fixing to actually adapt.

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello the name of the person who is running file");
+		System.out.println("Hello" + System.getProperty("user.name"));
 	}
 
 }
