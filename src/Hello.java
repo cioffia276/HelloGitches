6905c53b19254944b1262b805f89cb955d20d762
wingsteven <wings253@strose.edu>
2019-01-22 15:46:13 -0500

    Steve Editted this

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello" + System.getProperty("user.name"));
+		System.out.println("Hello, this is Steve Wing editting this file\n\n\n\n\n\npunks");
 	}
 
 }
